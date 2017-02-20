6ad26e5ef2abf8f68e326db03d346cf3cece3a16
bigelephant29 <dc9491@gmail.com>
2017-02-20 17:21:41 +0800

    Lab01 slide: add more description and sample code

---
+++ lab01/bad.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cin >> num;
+if(num == 2) {{
+if(num == 3) {
+{
+cout << "HAHA" << endl;
+}
+cout << "HEHE" << endl;
+}
+cout << "HEYHEY" << endl;
+}}}
+}
+if(num==4){
+}if(num==5){
+}}if(num==6){
+cout << "Hi" << endl;
+}
+return 0;
+}
---
+++ lab01/good.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+// <-- insert proper empty lines to seperate code
+int main()
+{  // <-- align paired braces vertically
+    int inputNum;  // <-- understandable names
+    cin >> inputNum;  // <-- start a new line after each semicolon
+    if(inputNum == 1)
+    {
+        cout << "Input Number is 1\n";
+    }
+    if(inputNum == 2)
+    {
+        cout << "Input Number is 2\n";
+    }
+    /* Write comments for your code
+     * English is better than Chinese while writing comments.
+     */
+    return 0;
+}
