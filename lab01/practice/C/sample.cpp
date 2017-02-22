34509ed1ad5059f5458b127e70235d9494912243
bigelephant29 <dc9491@gmail.com>
2017-02-22 09:52:10 +0800

    Lab01 practice: add sample code for C

---
+++ lab01/practice/C/sample.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int m, n, temp = 0;
+    cin >> m >> n ;
+    if(n < m) {
+        // swap n & m
+        temp = m;
+        m = n;
+        n = temp;
+    }
+    while(m % n!= 0) {
+        temp = n;
+        n = m % n;
+        m = temp;
+    }
+    cout << n;
+    return 0;
+}
